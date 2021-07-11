e1c76b27f10af3fbc4573564a00f3551c7ecd8ea
oscardelalanza <oscardelalanza@gmail.com>
2021-07-11 13:00:36 -0700

    styled ColumnContainer added

--- src/components/styled/styled.ts
+++ src/components/styled/styled.ts
@@ -8,4 +8,14 @@ export const AppContainer = styled.div`
   height: 100%;
   width: 100%;
   padding: 100%;
-`
+`;
+
+export const ColumnContainer = styled.div`
+  background-color: #ebecf0;
+  width: 300px;
+  min-height: 40px;
+  margin-right: 20px;
+  border-radius: 3px;
+  padding: 8px 8px;
+  flex-grow: 0;
+`;
