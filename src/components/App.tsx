40d21975efdcc6dcf66ca771a72ec2f1e077257a
oscardelalanza <oscardelalanza@gmail.com>
2021-07-11 16:56:16 -0700

    redering AppContainer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { AppContainer } from '../styled/styled';
+import { Column } from './Column';
+import { Card } from './Card';
 
 const App = () => {
   return(
     <AppContainer>
-      Columns here
+      <Column text="To-Do">
+        <Card text="Generate app scaffold"/>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript"/>
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing"/>
+      </Column>
     </AppContainer>
   )
 }
