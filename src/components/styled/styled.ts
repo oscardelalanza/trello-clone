5e4822049859e4fd867fa9e764ac38eb4a4c4a7d
oscardelalanza <oscardelalanza@gmail.com>
2021-07-11 13:38:36 -0700

    styled CardContainer added

--- src/components/styled/styled.ts
+++ src/components/styled/styled.ts
@@ -24,3 +24,13 @@ export const ColumnTitle = styled.div`
   padding: 6px 16px 12px;
   font-weight: bold;
 `;
+
+export const CardContainer = styled.div`
+  background-color: #fff;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  max-width: 300px;
+  border-radius: 3px;
+  box-shadow: #091e4240 0 1px 0 0;
+`;
