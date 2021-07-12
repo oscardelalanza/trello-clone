89f2128ac272c315c1690139750162b16c205876
oscardelalanza <oscardelalanza@gmail.com>
2021-07-11 23:48:48 -0700

    use ref added to input

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { NewItemFormContainer, NewItemButton, NewItemInput } from '../styled/styled';
+import { useFocus } from '../utils/useFocus';
 
 interface NewItemFormProps {
   onAdd(text: string): void
@@ -7,10 +8,11 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps): JSX.Element => {
   const [text, setText] = useState('');
+  const inputRef = useFocus();
 
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={e => setText(e.target.value)}/>
+      <NewItemInput value={text} onChange={e => setText(e.target.value)} ref={inputRef}/>
       <NewItemButton onClick={() => onAdd(text)} children="Create"/>
     </NewItemFormContainer>
   );
