27ae7ffc7cc510ae880b3e6bc7822a615de84d72
oscardelalanza <oscardelalanza@gmail.com>
2021-07-11 18:48:47 -0700

    styled NewItemButton added

--- src/styled/styled.ts
+++ src/styled/styled.ts
@@ -63,3 +63,13 @@ export const NewItemFormContainer = styled.div`
   width: 100%;
   align-items: flex-start;
 `;
+
+export const NewItemButton = styled.button`
+  background-color: #5aac44;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #fff;
+  padding: 6px 12px;
+  text-align: center;
+`;
