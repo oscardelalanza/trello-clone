560d22a941b911b9edb953313cabc56534e6853f
oscardelalanza <oscardelalanza@gmail.com>
2021-07-11 18:52:26 -0700

    styled NewItemInput

--- src/styled/styled.ts
+++ src/styled/styled.ts
@@ -73,3 +73,12 @@ export const NewItemButton = styled.button`
   padding: 6px 12px;
   text-align: center;
 `;
+
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e4240 0 1px 0 0; 
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`;
